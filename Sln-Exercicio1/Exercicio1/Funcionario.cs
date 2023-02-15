6bed6854b6ef1369be66f770d371e65024e69909
LuFloripaSC <luciano_stucker@estudante.sesisenai.org.br>
2023-02-15 19:51:20 -0300

    Exercicio 1

---
+++ Sln-Exercicio1/Exercicio1/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Funcionario
+    {
+        private int id;
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public void SalvarFuncionario()
+        {
+            Console.WriteLine($"Passou por aqui");
+            Console.WriteLine($"Id {id}");
+        }
+        public int GerarCodigo()
+        {
+            return 123456;
+        }
+
+
+    }
+}

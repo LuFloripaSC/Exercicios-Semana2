f2581035e902b440c8bef1830c13a33678c5f03a
LuFloripaSC <luciano_stucker@estudante.sesisenai.org.br>
2023-02-15 20:05:44 -0300

    Inclusão de Estâncias

--- Sln-Exercicio1/Exercicio1/Program.cs
+++ Sln-Exercicio1/Exercicio1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Exercicio1;
+
 Console.WriteLine("Hello, World!");
+
+Funcionario funcionario = new Funcionario();
+funcionario.Nome = "Luciano";
+funcionario.DataNascimento = DateTime.Now;
+funcionario.SalvarFuncionario();

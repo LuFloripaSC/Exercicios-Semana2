95ec3af3de2c36842eb2e63bb03102c204534a45
LuFloripaSC <luciano_stucker@estudante.sesisenai.org.br>
2023-02-17 18:55:56 -0300

    Conclusão Exercício2

---
+++ Sln-Exercicio2/Exercicio2/FichadeInscricao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    public class FichadeInscricao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool MenorIdade { get; set; }
+        public FichadeInscricao(int id, string nome, DateTime dataNascimento, bool menorIdade)
+
+        {
+            Id = id;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            MenorIdade = menorIdade;
+        }
+
+        public void MostrarCampos()
+        {
+            Console.WriteLine($"Id{Id}");
+            Console.WriteLine($"Nome {Nome}");
+            Console.WriteLine($"DataNascimento {DataNascimento}");
+                if (MenorIdade == false) Console.WriteLine("Maior de Idade");
+                else Console.WriteLine("Menor de Idade");
+           
+        }
+    }
+}
---
+++ Sln-Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Exercicio2;
+
+    
+        DateTime data = new DateTime(1977, 04, 17, 18, 14, 30);
+        FichadeInscricao fichadeInscricao = new FichadeInscricao(1, "Luciano", data, false);
+        fichadeInscricao.MostrarCampos();
+    

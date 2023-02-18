3f04237a10ce463b1c32041a263dfd54c07b5db8
LuFloripaSC <luciano_stucker@estudante.sesisenai.org.br>
2023-02-18 20:50:38 -0300

    Exercicio3

---
+++ Sln-Exercicio3/Exercicio3/MensagemCelular.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Exercicio3
+{
+    public class MensagemCelular
+    {
+        private int telefone;
+        private string mensagem;
+        public int Telefone { get; set; }
+
+        public string Mensagem { get; set; }
+
+        private void EnviarMensagemAoTelefone()
+        {
+            Console.WriteLine("Método privado executado na classe");
+            Console.WriteLine($"Telefone {Telefone}, Mensagem {Mensagem}");
+        }
+        public void Executar()
+        {
+            Console.WriteLine("Método Executado pelo Console");
+            EnviarMensagemAoTelefone();
+        }
+    }
+}
---
+++ Sln-Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,10 @@
+﻿using Exercicio3;
+    {
+  
+    {
+        MensagemCelular mensagem = new MensagemCelular();
+        mensagem.Telefone = 12345678;
+        mensagem.Mensagem = "O Exercicio Funcionou";
+        mensagem.Executar();
+    }
+}

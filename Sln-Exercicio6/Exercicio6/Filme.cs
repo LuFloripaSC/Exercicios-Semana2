9e80846a10d795b4847450034ed9673d84c21f52
LuFloripaSC <luciano_stucker@estudante.sesisenai.org.br>
2023-02-20 22:05:10 -0300

    Exercicio6

---
+++ Sln-Exercicio6/Exercicio6/Filme.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Exercicio6
+{
+    internal class Filme
+    {
+        public string NomeFilme;
+        public string Categoria;
+        public Filme(string nomeFilme, string categoria)
+        {
+            NomeFilme = nomeFilme;
+            Categoria = categoria;
+        }
+        public void Exibir()
+        {
+            Console.WriteLine($"Filme {NomeFilme}, Categoria {Categoria}");
+        }
+    }
+      
+
+}
---
+++ Sln-Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,4 @@
+﻿using Exercicio6;
+
+Filme filme = new Filme("De Volta para o Futuro", "Aventura e Ficção");
+filme.Exibir();

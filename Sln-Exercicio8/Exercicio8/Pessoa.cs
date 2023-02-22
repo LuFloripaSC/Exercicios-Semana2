32dddf16068f28b01990476d4b9b5978aefec1f0
LuFloripaSC <luciano_stucker@estudante.sesisenai.org.br>
2023-02-22 18:41:54 -0300

    Exercicio8

---
+++ Sln-Exercicio8/Exercicio8/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Exercicio8
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public void MostrarIdade()
+        {
+            int idade = CalcularIdade();
+            Console.WriteLine($"Nome {Nome} tem a Idade de {idade} anos");
+        }
+        private int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+            if (DataNascimento > dataAtual.AddYears(-idade))
+            {
+                idade--;
+            }
+            return idade;
+        }
+    }
+}
---
+++ Sln-Exercicio8/Exercicio8/Program.cs
@@ -0,0 +1,7 @@
+﻿using Exercicio8;
+
+Pessoa pessoa= new Pessoa();
+pessoa.Nome = "Luciano";
+pessoa.DataNascimento = new DateTime(1977, 4, 17);
+pessoa.MostrarIdade();
+
